b452a2bb404c3f4520a1f99948ed416cccc8dc6c
Eugene Jen <ejen@wgen.net>
2014-01-27 21:49:05 -0500

    add edit post and update method. use partial for blog post form

--- rails/blog/app/controllers/posts_controller.rb
+++ rails/blog/app/controllers/posts_controller.rb
@@ -1,24 +1,42 @@
 class PostsController < ApplicationController
-    def new
-    end
+  def new
+    @post = Post.new
+  end
 
-    def create
-        @post = Post.new(post_params)
-        @post.save
-        redirect_to @post
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
     end
+  end
 
-    def show
-        @post = Post.find(params[:id])
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:title, :text))
+      redirect_to @post
+    else
+      render 'edit'
     end
+  end
 
-    def index
-        @posts = Post.all
-    end
+  def show
+    @post = Post.find(params[:id])
+  end
 
-    def post_params
-        params.require(:post).permit(:title, :text)
-    end
+  def index
+    @posts = Post.all
+  end
 
-    private :post_params
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  private :post_params
 end
